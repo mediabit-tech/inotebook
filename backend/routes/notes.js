5cc288eb2caa07d57a81684b64c65905f146d57a
Vinay Tripathi <66164980+mediabit-tech@users.noreply.github.com>
2021-12-29 17:55:12 +0530

    Video 54 Completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -58,25 +58,54 @@ router.post('/addnote', fetchuser, [
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
     // Create a destructure of an array with title, description & tag for update the existsing note of same user into db
     const { title, description, tag } = req.body;
-    // Create a newNote object
-    const newNote = {};
-    if(title) { newNote.title = title };
-    if(description) { newNote.description = description };
-    if(tag) { newNote.tag = tag };
+    try {
+        // Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
 
-    // Find the note to be updated and update it
-    // Here params.id is pass by user id like '/updatenote/:id'
-    let note = await Note.findById(req.params.id);
-    // If note is not available
-    if(!note) { return res.status(404).send("Not found") };
-    // If unauthorised access is done
-    if(note.user.toString() !== req.user.id) { return res.status(401).send("Not allowed!") }
+        // Find the note to be updated and update it
+        // Here params.id is pass by user id like '/updatenote/:id'
+        let note = await Note.findById(req.params.id);
+        // If note is not available
+        if (!note) { return res.status(404).send("Not found") };
+        // If unauthorised access is done (only for authorised user)
+        if (note.user.toString() !== req.user.id) { return res.status(401).send("Not allowed!") }
 
-    // Update note
-    // Help of {new:true} if any new content is coming then it's insert directly into db
-    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
-    res.json({note});
+        // Update note
+        // Help of {new:true} if any new content is coming then it's insert directly into db
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error!");
+    }
 })
 
+
+
+
+
+// ROUTE 4 : Delete an existing Note using: DELETE "/api/notes/deletenote". Login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+        // Find the note to be deleted and delete it
+        // Here params.id is pass by user id like '/deletenote/:id'
+        let note = await Note.findById(req.params.id);
+        // If note is not available
+        if (!note) { return res.status(404).send("Not found") };
+        // If unauthorised access is done (only for authorised user)
+        if (note.user.toString() !== req.user.id) { return res.status(401).send("Not allowed!") }
+
+        // Update note
+        // Help of {new:true} if any new content is coming then it's insert directly into db
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({ "Success": "Note has been deleted!", note: note });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error!");
+    }
+})
 
 module.exports = router;

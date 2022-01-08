df64c11b650c4b3d6e615292c4123c6c49675956
Vinay Tripathi <66164980+mediabit-tech@users.noreply.github.com>
2022-01-08 16:30:48 +0530

    Video 70 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -16,11 +16,12 @@ router.post('/createuser', [
    body('email', 'Enter a valid email').isEmail(),
    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
 ], async (req, res) => {
+   let success = false;
    // If there are errors, return bad request and the errors
    const errors = validationResult(req);
    // Create a validation for error display
    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success, errors: errors.array() });
    }
    // Create user help of promise & send data into mongodb
    // Check whether the user with this email exists already
@@ -28,7 +29,7 @@ router.post('/createuser', [
       let user = await User.findOne({ email: req.body.email });
       // If find user then return bad request
       if (user) {
-         return res.status(400).json({ error: "Sorry! a user with this email  already exists" })
+         return res.status(400).json({ success, error: "Sorry! a user with this email  already exists" })
       }
 
       // Create a salt for secure the password help of bcrypt.js
@@ -50,7 +51,8 @@ router.post('/createuser', [
       }
       const authToken = jwt.sign(data, JWT_SECRET);
       // res.json(user);
-      res.json({ authToken });
+      success = true;
+      res.json({ success, authToken });
    } catch (error) {
       console.log(error.message);
       res.status(500).send("Internal server error!");
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
 const Signup = () => {
+
+    const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" })
+    let history = useHistory();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Distructuring
+        const { name, email, password } = credentials;
+        // Fetch API boiler plate
+        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+        const json = await response.json()
+        console.log(json);
+        if (json.success) {
+            // Save the authToken and redirect
+            localStorage.setItem("token", json.authToken);
+            history.push("/login");
+        } else {
+            alert("Invalid credentials!")
+        }
+    }
+
+    const onChange = (e) => {
+        // Spread operator
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    }
+
     return (
-        <div>
-            Signup page
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Full name</label>
+                    <input type="text" className="form-control" value={credentials.name} onChange={onChange} id="name" name="name" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} id="password" name="password" minLength={5} required/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="cpassword" className="form-label">Confirm password</label>
+                    <input type="cpassword" className="form-control" value={credentials.cpassword} onChange={onChange} id="cpassword" name="cpassword" minLength={5} required/>
+                </div>
+                <button type="submit" className="btn btn-primary">Signup</button>
+            </form>
+        </>
     )
 }
 
